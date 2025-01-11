4e1de007f9cffe413f5eeb006a7ea05cc0285264
Jude <jm2489@njit.edu>
2025-01-11 16:28:05 -0500

    Got an unexpected ouput from the previous ver. This should work compared to the other sorting algorithms.

--- Project3/srtheap.c
+++ Project3/srtheap.c
@@ -6,12 +6,13 @@ void srtheap(void *base, size_t nelem, size_t size, int (*compar)(const void *, 
     char *qb = (char *)base;
 
     // Max heap
-    for (size_t i = nelem / 2; i-- > 0;) {
-        size_t root = i;
+    for (int i = (int)(nelem / 2) - 1; i >= 0; i--) {
+        int root = i;
 
-        while (2 * root + 1 < nelem) {
-            size_t child = 2 * root + 1;
-            if (child + 1 < nelem && compar(qb + child * size, qb + (child + 1) * size) < 0) {
+        while (2 * root + 1 < (int)nelem) {
+            int child = 2 * root + 1;
+
+            if (child + 1 < (int)nelem && compar(qb + child * size, qb + (child + 1) * size) < 0) {
                 child++;
             }
 
@@ -25,19 +26,21 @@ void srtheap(void *base, size_t nelem, size_t size, int (*compar)(const void *, 
     }
 
     // Heap sort
-    for (size_t i = nelem - 1; i > 0; --i) {
+    for (int i = (int)nelem - 1; i > 0; i--) {
         swap(qb, qb + i * size, size);
 
-        size_t root = 0;
+        int root = 0;
+
         while (2 * root + 1 < i) {
-            size_t child = 2 * root + 1;
+            int child = 2 * root + 1;
+
             if (child + 1 < i && compar(qb + child * size, qb + (child + 1) * size) < 0) {
                 child++;
             }
 
             if (compar(qb + root * size, qb + child * size) < 0) {
                 swap(qb + root * size, qb + child * size, size);
-                root = child; 
+                root = child;
             } else {
                 break;
             }
